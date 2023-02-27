52977abbfe6a4cb55abfd65f428c7f4f3ae8b101
Carl Bourne <carl.bourne@mac.com>
2023-02-27 15:32:30 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+import type { PopupSettings } from '@skeletonlabs/skeleton';	
+
+let exampleSettings: PopupSettings = {
+	// Set the event as: click | hover | hover-click
+	event: 'click',
+	// Provide a matching 'data-popup' value.
+	target: 'examplePopup'
+};
+			
+
+
+</script>
+<h1>Test</h1>
+
+<button class="btn variant-filled" use:popup={exampleSettings}>
+	<span>Menu</span>
+	<span class="badge bg-white/10 dark:bg-black/10">Tap</span>
+</button>
