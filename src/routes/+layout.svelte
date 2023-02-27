a852101c14a05fcb8cbd1409ddfa1f317f3b10c1
Carl Bourne <carl.bourne@mac.com>
2023-02-27 15:37:07 +0000

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,12 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	import { popup } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
 </script>
 
 <!-- App Shell -->
