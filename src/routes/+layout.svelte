a01c44eba2d8bb00a31a875e173cdb3b581360b4
Carl Bourne <carl.bourne@mac.com>
2023-02-27 22:41:18 +0000

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
-	import { popup } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
 
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 
 import type { PopupSettings } from '@skeletonlabs/skeleton';	
-import type { popup } from '@skeletonlabs/skeleton';	
+import { popup } from '@skeletonlabs/skeleton';	
 
 
 let exampleSettings: PopupSettings = {
@@ -11,12 +11,25 @@ let exampleSettings: PopupSettings = {
 	target: 'examplePopup'
 };
 			
-
-
 </script>
 <h1>Test</h1>
 
-<button class="btn variant-filled" use:popup={exampleSettings}>
-	<span>Menu</span>
-	<span class="badge bg-white/10 dark:bg-black/10">Tap</span>
-</button>
+<div>
+	<button class="btn variant-filled" use:popup={exampleSettings}>
+		<span>Menu</span>
+		<span class="badge bg-white/10 dark:bg-black/10">Tap</span>
+	</button>
+	<div class="card variant-filled-secondary p-4 w-72 shadow-xl" data-popup="exampleMenu">
+		<!-- NOTE: Keep this wrapper, .space-y will affect the arrow -->
+		<div class="space-y-4">
+			<p class="font-bold">This is a <strong>Menu</strong> example.</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta amet nam hic aspernatur cum porro praesentium. Voluptates
+				velit ex ad eius sit! Sit deserunt ex accusamus quod fugit enim in?
+			</p>
+			<button class="btn variant-filled">Close Me</button>
+		</div>
+		<!-- Arrow -->
+		<div class="arrow variant-filled-secondary" />
+	</div>
+</div>
